8f35b3a73d6785e40638266871ce50daa9e0decc
Peter Shin <petershintech@gmail.com>
2020-10-13 22:56:50 +1100

    Add tests to check parameter or state values

--- test/test_LinearBucket.jl
+++ test/test_LinearBucket.jl
@@ -1,8 +1,15 @@
 
 @testset "LinearBucket.jl" begin
-    @testset "Test with invalid values" begin
+    @testset "Check param or state values when creating new instance" begin
         @test_throws DomainError LinearBucket(-0.1, 0.0)
         @test_throws DomainError LinearBucket(1.1, 0.0)
         @test_throws DomainError LinearBucket(0.1, -0.5)
+    end
+
+    @testset "Check values when a parameter or state value is set" begin
+        model = LinearBucket(0.1, 0.0)
+        @test_throws DomainError model.K = -0.1
+        @test_throws DomainError model.K = 1.1
+        @test_throws DomainError model.S = -0.5
     end
 end
